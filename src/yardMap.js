3d52f211a6858ad929b31ffd31b6cd0c5391567a
AngryPanda <287036406@qq.com>
2019-06-10 17:30:37 +0800

    增加点击 增加雷达图

--- src/yardMap.js
+++ src/yardMap.js
@@ -20,10 +20,12 @@ const mapConfig = {
     left: 24,
     right: 24
   },
-  radarSeatWidth: 4,
-  radarSeatHeight: 4,
-  radarSeatMargin: 2,
-  siteDistance: 45
+  radarSiteWidth: 4,
+  radarSiteHeight: 4,
+  radarSiteMargin: 2,
+  siteSelectedColor: '#389e0d',
+  siteColor: '#fff',
+  siteCtnColor: '#fa541c'
 }
 
 // 获得dpr
@@ -50,40 +52,50 @@ class YardMap {
     this.xindex = $('.x-index');
     this.yindex = $('.y-index');
     this.indexItems = $('#index-items').html();
+    this.radarElement = document.querySelector('.radar');
+    this.radarCanvas = document.querySelector('.canvas-radar');
+    this.radarCtx = this.radarCanvas.getContext('2d');
+    this.radarFrame = document.querySelector('.radar-frame')
     this.siteWidth = this.mapConfig.siteWidth; //每个位置的宽
     this.siteHeight = this.mapConfig.siteHeight; //每个位置的高
     this.siteMargin = this.mapConfig.siteMargin; //每个位置的间距
+    this.viewWidth = this.mapConfig.viewWidth ||  window.innerWidth; //整个视图宽
+    this.viewHeight = this.mapConfig.viewHeight || window.innerHeight; //整个视图高
     this.data = this.mapConfig.data || {};
+    this.maxRadarWidth = 0.5 * this.viewWidth;
     this.dy = 0; //地图y轴位移
     this.dx = 0; //地图x轴位移
     this.scale = 1; //初始缩放
     this.maxScale = 1; //最大缩放
+    this.radarScale = 1; //雷达图初始缩放
     this.dpr = getPixelRatio(); //地图像素比
-    this.viewWidth = window.innerWidth; //整个视图宽
-    this.viewHeight = window.innerHeight; //整个视图高
     this.mapData = this.getMapData(this.data);
     this.mapWidth = this.mapData.mapWidth; //地图的宽度
     this.mapHeight = this.mapData.mapHeight; //地图的高度
+    this.radarWidth = this.mapData.radarWidth; //雷达宽度
+    this.radarHeight = this.mapData.radarHeight; //雷达高度
     const mapOffset = $(this.mapCanvas).offset();
     this.mapOffset = {
       ...mapOffset
     }
+    this.selectedSites = []; // 选中的位置
+    this.selectedPrevSite = {};
     this.initScale();
     this.createIndex();
     this.bindEvent();
     this.init();
   }
 
-
-
   //初始化
   init() {
     this.initSize();
+    this.initRadar();
     this.setMatrix({
       dx: Math.round((this.viewWidth - this.mapWidth*this.scale) / 2),
       dy: this.mapConfig.offset.dy
     });
     this.renderMap(this.mapCtx);
+    this.renderRadar(this.radarCtx);
   }
 
   initSize() {
@@ -121,9 +133,11 @@ class YardMap {
   //解析地图的数据
   getMapData(data){
     const { maxColumn, maxRow, minColumn, minRow, site } = data;
-    const { siteWidth, siteMargin, siteHeight, padding } = this.mapConfig;
+    const { siteWidth, siteMargin, siteHeight, padding, radarSiteWidth, radarSiteHeight, radarSiteMargin } = this.mapConfig;
     const mapWidth = siteWidth * maxColumn + siteMargin * (maxColumn - 1) + padding.left + padding.right; //地图的宽度 = 位置的宽度*列数 + 位置间距*（列数-1）+ 左padding + 右padding
-    const mapHeight = siteHeight * maxRow + siteMargin * (maxRow - 1) + padding.top
+    const mapHeight = (siteHeight+siteMargin) * maxRow + padding.top;
+    const radarWidth = radarSiteWidth * maxColumn + radarSiteMargin * (+maxColumn+1);
+    const radarHeight = (radarSiteHeight+radarSiteMargin) * maxRow;
     return {
       maxColumn,
       minColumn,
@@ -131,17 +145,33 @@ class YardMap {
       minRow,
       mapWidth,
       mapHeight,
+      radarWidth,
+      radarHeight,
       site: site.map(item => {
         const { column, row } = item;
         const vx = (siteWidth + siteMargin ) * (column - 1) + padding.left;
         const vy = (siteHeight + siteMargin) * (row - 1) + padding.top;
+        const rx  = (radarSiteWidth + radarSiteMargin) * (column-1) + radarSiteMargin;
+        const ry = (radarSiteHeight + radarSiteMargin) * (row - 1) + radarSiteMargin
         return {
           ...item,
           vx,
-          vy
+          vy,
+          rx,
+          ry
         }
       })
     }
+  }
+
+  //得到点击位置
+  getTapedSite(centerX, centerY) {
+    return this.mapData.site.filter(item => {
+      const { vx, vy } = item;
+      const toRight = vx + this.mapConfig.siteWidth;
+      const toBottom = vy + this.mapConfig.siteHeight;
+      return vx <= centerX && centerX <= toRight && vy <= centerY && centerY <= toBottom;
+    })
   }
 
   //得到索引
@@ -191,6 +221,32 @@ class YardMap {
     this.scale = scale;
   }
 
+  //雷达图初始化
+  initRadar() {
+    if(!this.mapConfig.radar) return;
+    let radarWidth = this.radarWidth,
+        radarHeight = this.radarHeight;
+        
+    if(radarWidth > radarHeight && radarWidth > this.maxRadarWidth) {
+      this.radarScale = this.maxRadarWidth / radarWidth;
+      radarHeight*=this.radarScale;
+      radarWidth = this.maxRadarWidth;
+    } else {
+      if(radarHeight > radarWidth && radarHeight > this.maxRadarWidth) {
+        this.radarScale = this.maxRadarWidth / radarHeight;
+        radarWidth*=this.radarScale;
+        radarHeight = this.maxRadarWidth;
+      }
+    }
+    this.radarWidth = radarWidth;
+    this.radarHeight = radarHeight;
+    this.radarCanvas.width = radarWidth * this.dpr;
+    this.radarCanvas.height = radarHeight * this.dpr;
+    this.radarCanvas.style.width = `${this.radarWidth}px`;
+    this.radarCanvas.style.height = `${this.radarHeight}px`;
+    this.radarCtx.scale(this.dpr * this.radarScale, this.dpr * this.radarScale);
+  }
+
   //绑定手势
   bindEvent() {
     this.mc = new Hammer.Manager(this.mapCanvas);
@@ -202,6 +258,7 @@ class YardMap {
       this.startDx = this.dx;
     })
     this.mc.on('panmove', e => {
+      this.showRadar();
       this.setMatrix({
         scale: this.scale,
         dx: this.startDx + e.deltaX,
@@ -243,20 +300,89 @@ class YardMap {
 
   handleMapTap = e => {
     e.preventDefault();
-    const _dx = e.center.x - this.mapOffset.left - this.dx,
-          _dy = e.center.y - this.mapOffset.top - this.dy,
-          _scaleX = _dx / this.scale,
-          _scaleY = _dy / this.scale;
-    this.scale !== 1 &&
-    this.setMatrix({
-      scale: 1,
-      dx: this.dx + Math.round(_dx - _scaleX),
-      dy: this.dy + Math.round(_dy - _scaleY)
-    });
-    this.checkBoundary();
-    this.updateMap();
+    const centerX = e.center.x - this.mapOffset.left - this.dx,
+          centerY = e.center.y - this.mapOffset.top - this.dy,
+          scaleCenterX = centerX / this.scale,
+          scaleCenterY = centerY / this.scale,
+          tapedSite = this.getTapedSite(scaleCenterX, scaleCenterY)[0];
+    if(this.scale !== 1) {
+      this.setMatrix({
+        scale: 1,
+        dx: this.dx + Math.round(centerX - scaleCenterX),
+        dy: this.dy + Math.round(centerY - scaleCenterY)
+      });
+      this.checkBoundary();
+      this.renderMap(this.mapCtx);
+      this.updateRadar();
+    }
+    this.showRadar();
+    this.updateIndex();
+    if(tapedSite) {
+      this.selectSeats(tapedSite);
+    }
   }
 
+  //位置选中
+  selectSeats(site) {
+    let selectState = '';
+    selectState = this.isSelected(site) ? 'dselected' : 'selected';
+    if(selectState === 'dselected' || selectState === 'selected') {
+      selectState === 'selected' ? this.addSeletedSite(site) : this.removeSelectedSite(site);
+      this.renderMap(this.mapCtx);
+      this.renderRadar(this.radarCtx);
+      this.selectedSites.forEach(item => {
+        this.renderSite(this.mapCtx, { 
+          color: this.getSiteColor(item), 
+          site: item,
+          redraw: true
+        });
+        this.mapConfig.radar && this.renderRadarSite(this.radarCtx, {
+          color: this.getSiteColor(item),
+          site: item,
+          redraw: true
+        })
+      })
+      /* if(this.selectedPrevSite) {
+        
+      }
+      if(this.selectedSites[0]) {
+        this.renderSite(this.mapCtx, { 
+          color: this.getSiteColor(this.selectedSites[0]), 
+          site: this.selectedSites[0], 
+          redraw: true 
+        });
+        this.mapConfig.radar && this.renderRadarSite(this.radarCtx, {
+          color: this.getSiteColor(this.selectedSites[0]),
+          site: this.selectedSites[0]
+        })
+      }
+      this.selectedPrevSite = site; */
+      this.mapConfig.onSelected && this.mapConfig.onSelected.call(this, this.selectedSites, selectState);
+    }
+  }
+
+  isSelected(site) {
+    return this.selectedSites.some(item => {
+      return item.siteId === site.siteId
+    });
+  }
+
+  addSeletedSite(site) {
+    if(!this.isSelected(site)) {
+      this.selectedSites[0] = site;
+    }
+  }
+
+  removeSelectedSite(site) {
+    this.selectedSites = this.selectedSites.filter(item => {
+      return item.siteId !== site.siteId;
+    })
+  }
+
+  canSelect(site) {
+    return site.status === '1';
+  }
+  
   //检测边界
   checkBoundary() {
     let thisDx = this.dx,
@@ -300,14 +426,56 @@ class YardMap {
       ctx.restore();
       ctx.setTransform(this.scale * this.dpr, 0, 0, this.scale * this.dpr, this.dx * this.dpr, this.dy * this.dpr) //设置画布的缩放与位移
       this.mapData.site.forEach(item => {
-        this.renderSite(ctx, item)
+        this.renderSite(ctx, {
+          color: this.getSiteColor(item),
+          site: item
+        })
       });
       ctx.save();
     }
   }
 
+  //渲染雷达图
+  renderRadar(ctx) {
+    if(!this.mapConfig.radar) return;
+    this.mapData.site.forEach(item => {
+      this.renderRadarSite(ctx, {
+        color: this.getSiteColor(item),
+        site: item
+      })
+    });
+  }
+
+  //渲染雷达位置
+  renderRadarSite(ctx, renders) {
+    const { site, color, redraw } = renders;
+    const { rx, ry } = site;
+    ctx.fillStyle = color;
+    if(redraw) {
+      ctx.clearRect(rx, ry, this.mapConfig.radarSiteWidth, this.mapConfig.radarSiteHeight);
+      ctx.fillRect(rx, ry, this.mapConfig.radarSiteWidth, this.mapConfig.radarSiteHeight);
+    } else {
+      ctx.fillRect(rx, ry, this.mapConfig.radarSiteWidth, this.mapConfig.radarSiteHeight)
+    }
+  }
+
+  //渲染贝位
+  renderSite(ctx, renders) {
+    const { site, color, redraw } = renders;
+    const { vx, vy } = site;
+    ctx.fillStyle = color;
+    if(redraw) {
+      ctx.clearRect(vx, vy, this.mapConfig.siteWidth, this.mapConfig.siteHeight);
+      ctx.fillRect(vx, vy, this.mapConfig.siteWidth, this.mapConfig.siteHeight);
+    } else {
+      ctx.fillRect(vx, vy, this.mapConfig.siteWidth, this.mapConfig.siteHeight)
+    }
+   
+  }
+
   //更新整个map 包括坐标轴等
   updateMap() {
+    this.updateRadar();
     this.renderMap(this.mapCtx);
     this.updateIndex();
   }
@@ -326,9 +494,36 @@ class YardMap {
     this.xindex.find('ul>li').css('width', (this.siteWidth+this.siteMargin)*scale);
   }
 
-  //渲染贝位
-  renderSite(ctx, site) {
-    ctx.fillStyle = site.status === '1' ? '#fff' : '#fa541c';
-    ctx.fillRect(site.vx, site.vy, this.mapConfig.siteWidth, this.mapConfig.siteHeight)
+  //更新雷达
+  updateRadar() {
+    if(!this.mapConfig.radar) return 
+    let scaleMapWidth = this.mapWidth * this.scale,
+        scaleMapHeight = this.mapHeight * this.scale,
+        viewWidth = this.viewWidth,
+        viewHeight = this.viewHeight,
+        widthRatio = viewWidth / scaleMapWidth,
+        heightRatio = viewHeight / scaleMapHeight,
+        dx = -this.dx / scaleMapWidth * this.radarWidth,
+        dy = -(this.dy-this.mapConfig.offset.dy) / scaleMapHeight * this.radarHeight;
+    $(this.radarFrame).css({
+      width: this.radarWidth * (widthRatio > 1 ? 1 : widthRatio),
+      height: this.radarHeight*(heightRatio > 1 ? 1 : heightRatio),
+      transform: `translate3d(${dx}px, ${dy}px,0)`
+    })
   }
+
+  //显示雷达图
+  showRadar() {
+    if(!this.mapConfig.radar) return;
+    this.timer && clearTimeout(this.timer);
+    $(this.radarElement).addClass('active');
+    this.timer = setTimeout(()=>{
+      $(this.radarElement).removeClass('active');
+    }, 2000)
+  }
+  getSiteColor(site) {
+    const { siteColor, siteCtnColor, siteSelectedColor } = this.mapConfig;
+    return this.isSelected(site) ? siteSelectedColor : site.status == 1 ? siteColor : siteCtnColor;
+  }
+  
 }
